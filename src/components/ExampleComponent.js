219611a5a66c2e937742a29eb94ed8a3cefb4fac
Ahamefule Onyegbule <aonyeobi@gmail.com>
2025-01-27 14:27:10 -0500

    Complete lab

--- src/components/ExampleComponent.js
+++ src/components/ExampleComponent.js
@@ -3,7 +3,8 @@ import React from "react";
 // no need to modify anything in here. The second test is looking for the returned JSX from this component
 function ExampleComponent() {
   return (
-    <div className="example">
+    <div>
+      <h3>Wow!</h3>
       <img
         src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif"
         alt="a deeply impressed man, perhaps thinking 'whoa'"
